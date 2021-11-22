507fde9816a272132d543e4766058dbd66f6306c
Sola Otudeko <sotudeko@sonatype.com>
2021-11-22 12:56:52 +0000

    9

--- releasefiles/reports2/make-exe.sh
+++ releasefiles/reports2/make-exe.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
 #pip3 install pyinstaller
+#PyInstaller: 4.7
+#Python: 3.9.8
+#docker run -v "$(pwd):/src/" cdrx/pyinstaller-linux "pyinstaller reports2.py"
+
 pyinstaller --name sm-reports --exclude-module=pytest --clean --onefile --console --noconfirm reports2.py
+
 
